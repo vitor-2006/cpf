84bfd16cc963455599bf339d5289b60febfb72ac
vitor-2006 <vitorbalbinotdellatorre@gmail.com>
2025-08-14 09:36:48 -0300

    cpf

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+
+import express, {Request, Response} from 'express'
+import validationBr from 'validation-br'
+
+const app = express();
+const port: number = 3000;
+
+app.use(express.json());
+	
+app.listen(port, () => {
+    console.log("Api iniciada na porta: " + port);
+});
+
+app.use(express.json())
+
+app.get('/valida-cpf/:cpf', (req: Request, res: Response) => {
+    if(validationBr.isCPF(req.params.cpf)) {
+        return res.send('CPF válido')
+    }else{
+        return res.send('CPF inválido')
+    }
+})
+
